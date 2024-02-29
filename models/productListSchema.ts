c5c7611ba3f6ef1bc446959dd2328ae16f55ae7b
Seny Irinarkhova <kseniiaIrinarkhova@gmail.com>
2024-02-28 19:24:33 -0800

    Added productList schema

---
+++ models/productListSchema.ts
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+const productListSchema:Schema = new mongoose.Schema({
+user_id:{
+    type: Number, // have to change for link!!
+        required: true
+},
+store_id:{
+    type: Schema.Types.ObjectId,
+    ref: 'Store',
+    required: true
+},
+products:{
+    type : [{
+        product_id :{
+            type: Schema.Types.ObjectId,
+            ref: 'Product'
+        },
+        amount : Number, // required in DB validation
+        price: Number // required in DB validation
+    }]
+}
+});
+
+export default mongoose.model("ProductList", productListSchema);
